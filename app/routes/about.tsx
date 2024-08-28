3ec4c3288c368453873a97a38c7d140b718b682b
Norman Qian <normanqian@gmail.com>
2024-08-28 15:16:02 -0400

    routedebug

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,11 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "About Us" },
-    { name: "description", content: "Learn more about our company" },
-  ];
-};
+import React from "react";
 
 export default function About() {
   return (
