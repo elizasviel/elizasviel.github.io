27b76cc6c8884b535ae255106083e355e0e6888f
Norman Qian <normanqian@gmail.com>
2024-08-28 12:04:57 -0400

    tweaks

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,10 +1,7 @@
 import { useEffect } from "react";
-import { useNavigate } from "@remix-run/react";
 import resumePdf from "../images/Norman Qian Resume 2024 V.1.pdf";
 
 export default function Resume() {
-  const navigate = useNavigate();
-
   useEffect(() => {
     window.location.href = resumePdf;
   }, []);
