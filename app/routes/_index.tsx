a1810e2dc385055963219584a967afed03975a7d
Norman Qian <normanqian@gmail.com>
2024-08-28 16:33:26 -0400

    GITHUB PAGES DOES NOT SUPPORT SPA ROUTING

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -182,7 +182,7 @@ export default function Home() {
             normanqian@gmail.com
           </a>
           <a
-            href="/resume"
+            href={resumePdf}
             style={{ textDecoration: "none", color: "lightgray" }}
           >
             View Resume
@@ -227,7 +227,7 @@ const BlackboardLG = () => {
           link="mailto:normanqian@gmail.com"
         />
         <br />
-        <ProjectItem text="View Resume" link="/resume" />
+        <ProjectItem text="View Resume" link={resumePdf} />
       </div>
     </>
   );
