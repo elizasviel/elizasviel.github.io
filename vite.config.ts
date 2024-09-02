06e214cd6211452f3044810e785c041c9e94a929
Norman Qian <normanqian@gmail.com>
2024-09-01 20:52:50 -0400

    resetvite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/eliasviel.github.io/",
   plugins: [react()],
-  base: "/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html",
-        resume: "public/resume.html",
-      },
-    },
-  },
 });
